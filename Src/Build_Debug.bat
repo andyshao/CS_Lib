e1b169fd7d484d39ec7a865f37577fdb6ac1367c
free <free@AuxPC.com>
2014-07-24 00:06:20 +0800

    开始搭建基础的目录结构

---
+++ Src/Build_Debug.bat
@@ -0,0 +1,7 @@
+@attrib -R "..\Publish\*.*" /S /D
+
+@cd Library
+@call Build_Debug.bat
+
+@echo ±àÒëÍê±Ï
+@pause
---
+++ Src/Build_Release.bat
@@ -0,0 +1,7 @@
+@attrib -R "..\Publish\*.*" /S /D
+
+@cd Library
+@call Build_Release.bat
+
+@echo ±àÒëÍê±Ï
+@pause
